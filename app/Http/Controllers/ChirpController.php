1ba2688729999097a6914e13cfe1040df1f094c9
Aliu Adedigba <indexcorpng@gmail.com>
2024-07-09 08:30:26 +0100

    saving chirp

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Http\RedirectResponse;
 use App\Models\Chirp;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
@@ -26,11 +26,17 @@ class ChirpController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request): RedirectResponse
     {
         //
-    }
+        $validated = $request->validate([
+            'message' => 'required|string|max:255',
+        ]);
 
+        $request->user()->chirps()->create($validated);
+
+        return redirect(route('chirps.index'));
+    }
     /**
      * Display the specified resource.
      */
